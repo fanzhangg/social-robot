d6ceff7b5ae801949b066b0c34c246410b33e51b
fanzhangg <vanadiumzhang@gmail.com>
2020-05-08 15:21:08 -0500

    Add start, stop buttons (WIP)

--- gui.py
+++ gui.py
@@ -1,34 +1,35 @@
 from tkinter import *
 import cv2
+from face import Face
 
 
-class Face:
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.hasAnimation = False
-
-        canvas.create_oval(70, 70, 350, 350, fill='#FEE230', width=5, outline="#ED950D")
-        canvas.create_oval(110, 110, 200, 200, fill='white', tags='left', width=5, outline="#ED950D")
-        canvas.create_oval(110+30, 110+30, 110+60, 110+60, fill="black", tag='leftBall')
-        canvas.create_oval(310-90, 110, 310, 110+90, fill='white', tags='right', width=5, outline="#ED950D")
-        canvas.create_oval(220+30, 110+30, 220+60, 110+60, fill="black", tag="rightBall")
-        canvas.create_line(145, 250, 275, 250, width=5, tags='mouth')
-
-    def smile(self):
-        self.canvas.delete('mouth')
-        self.canvas.create_arc(125, 225, 275, 275, extent=-180, width=5, fill='white', tags='mouth')
-
-    def sad(self):
-        self.canvas.delete('mouth')
-        self.canvas.create_arc(125, 250, 275, 300, extent=180, width=5, fill='white', tags='mouth')
-
-    def wink(self):
-        self.canvas.delete('mouth')
-        self.canvas.create_line(125, 250, 275, 250, width=5, tags='mouth')
-
-    def grin(self):
-        self.canvas.delete('mouth')
-        self.canvas.create_line(125, 250, 200, 250, 275, 215, width=5, smooth='true', tags='mouth')
+# class Face:
+#     def __init__(self, canvas):
+#         self.canvas = canvas
+#         self.hasAnimation = False
+#
+#         canvas.create_oval(70, 70, 350, 350, fill='#FEE230', width=5, outline="#ED950D")
+#         canvas.create_oval(110, 110, 200, 200, fill='white', tags='left', width=5, outline="#ED950D")
+#         canvas.create_oval(110+30, 110+30, 110+60, 110+60, fill="black", tag='leftBall')
+#         canvas.create_oval(310-90, 110, 310, 110+90, fill='white', tags='right', width=5, outline="#ED950D")
+#         canvas.create_oval(220+30, 110+30, 220+60, 110+60, fill="black", tag="rightBall")
+#         canvas.create_line(145, 250, 275, 250, width=5, tags='mouth')
+#
+#     def smile(self):
+#         self.canvas.delete('mouth')
+#         self.canvas.create_arc(125, 225, 275, 275, extent=-180, width=5, fill='white', tags='mouth')
+#
+#     def sad(self):
+#         self.canvas.delete('mouth')
+#         self.canvas.create_arc(125, 250, 275, 300, extent=180, width=5, fill='white', tags='mouth')
+#
+#     def wink(self):
+#         self.canvas.delete('mouth')
+#         self.canvas.create_line(125, 250, 275, 250, width=5, tags='mouth')
+#
+#     def grin(self):
+#         self.canvas.delete('mouth')
+#         self.canvas.create_line(125, 250, 200, 250, 275, 215, width=5, smooth='true', tags='mouth')
 
 
 class GUI:
@@ -41,12 +42,13 @@ class GUI:
         self.face = None
         self.canvas = None
 
-    def setupWidgets(self):
+    def setup_widgets(self):
         """Set up all the parts of the GUI"""
-        self._initFace()
-        self._initExpressionTool()
+        self._init_face()
+        self._init_expression_tool()
+        self._init_video_tool()
 
-    def _initFace(self):
+    def _init_face(self):
         """Set up the face"""
         canvasFrame = Frame(self.root)
         canvasFrame.grid(row=1, column=1, padx=5, pady=5)
@@ -55,7 +57,16 @@ class GUI:
 
         self.face = Face(self.canvas)
 
-    def _initExpressionTool(self):
+    def _init_video_tool(self):
+        videoFrame = Frame(self.root, bd=5, padx=10, pady=10)
+        videoFrame.grid(row=2, column=1, padx=5, pady=5)
+        self.runBtn = Button(videoFrame, text="Start", command=self.face.run)
+        self.runBtn.grid(row=1, column=1)
+
+        self.stopBtn = Button(videoFrame, text="Stop")
+        self.stopBtn.grid(row=1, column=2)
+
+    def _init_expression_tool(self):
         expressionFrame = Frame(self.root, bd=5, padx=10, pady=10)
         expressionFrame.grid(row=1, column=2, padx=5, pady=5, sticky=N)
         expressionTitle = Label(expressionFrame, text="Expression Option", font="Arial 16 bold")
@@ -70,7 +81,7 @@ class GUI:
 
         self.smileBtn = Button()
 
-    def runFaceAnimation(self):
+    def run_face_animation(self):
         """
         This callback for the Start button moves the eyeball and change the facial emotion
         :return:
@@ -110,7 +121,7 @@ class GUI:
 
 def run():
     gui = GUI()
-    gui.setupWidgets()
+    gui.setup_widgets()
     gui.run()
 
 

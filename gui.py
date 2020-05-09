70cac9b0f77ee5edc40be41c456462ff36e99b33
fanzhangg <vanadiumzhang@gmail.com>
2020-05-08 22:09:51 -0500

    Remove face gui, bind method to run button

--- gui.py
+++ gui.py
@@ -1,36 +1,7 @@
 from tkinter import *
 import cv2
 from face import Face
-
-
-# class Face:
-#     def __init__(self, canvas):
-#         self.canvas = canvas
-#         self.hasAnimation = False
-#
-#         canvas.create_oval(70, 70, 350, 350, fill='#FEE230', width=5, outline="#ED950D")
-#         canvas.create_oval(110, 110, 200, 200, fill='white', tags='left', width=5, outline="#ED950D")
-#         canvas.create_oval(110+30, 110+30, 110+60, 110+60, fill="black", tag='leftBall')
-#         canvas.create_oval(310-90, 110, 310, 110+90, fill='white', tags='right', width=5, outline="#ED950D")
-#         canvas.create_oval(220+30, 110+30, 220+60, 110+60, fill="black", tag="rightBall")
-#         canvas.create_line(145, 250, 275, 250, width=5, tags='mouth')
-#
-#     def smile(self):
-#         self.canvas.delete('mouth')
-#         self.canvas.create_arc(125, 225, 275, 275, extent=-180, width=5, fill='white', tags='mouth')
-#
-#     def sad(self):
-#         self.canvas.delete('mouth')
-#         self.canvas.create_arc(125, 250, 275, 300, extent=180, width=5, fill='white', tags='mouth')
-#
-#     def wink(self):
-#         self.canvas.delete('mouth')
-#         self.canvas.create_line(125, 250, 275, 250, width=5, tags='mouth')
-#
-#     def grin(self):
-#         self.canvas.delete('mouth')
-#         self.canvas.create_line(125, 250, 200, 250, 275, 215, width=5, smooth='true', tags='mouth')
-
+from face_recognizer import FaceRecognizer
 
 class GUI:
     """Set up and manage all the variables for the GUI interface"""
@@ -60,7 +31,7 @@ class GUI:
     def _init_video_tool(self):
         videoFrame = Frame(self.root, bd=5, padx=10, pady=10)
         videoFrame.grid(row=2, column=1, padx=5, pady=5)
-        self.runBtn = Button(videoFrame, text="Start", command=self.face.run)
+        self.runBtn = Button(videoFrame, text="Start", command=self.run_animation)
         self.runBtn.grid(row=1, column=1)
 
         self.stopBtn = Button(videoFrame, text="Stop")
@@ -80,6 +51,10 @@ class GUI:
         self.grinBtn.grid(row=3, column=1)
 
         self.smileBtn = Button()
+
+    def run_animation(self):
+        rc = FaceRecognizer(self.root, self.canvas)
+        rc.start()
 
     def run_face_animation(self):
         """
